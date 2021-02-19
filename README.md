e82607858d300417dace30d0e683de430ab33105
Nolan Nordlund <nolan@projectrebel.io>
2021-02-19 12:07:07 -0600

    Removing migrations

--- README.md
+++ README.md
@@ -16,13 +16,6 @@ You can install the package via composer:
 composer require projectrebel/blink
 ```
 
-You can publish and run the migrations with:
-
-```bash
-php artisan vendor:publish --provider="ProjectRebel\Blink\BlinkServiceProvider" --tag="blink-migrations"
-php artisan migrate
-```
-
 You can publish the config file with:
 ```bash
 php artisan vendor:publish --provider="ProjectRebel\Blink\BlinkServiceProvider" --tag="blink-config"
--- src/BlinkServiceProvider.php
+++ src/BlinkServiceProvider.php
@@ -10,16 +10,10 @@ class BlinkServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('blink')
             ->hasConfigFile()
             ->hasViews()
-            ->hasMigration('create_blink_table')
             ->hasCommand(BlinkCommand::class);
     }
 }
